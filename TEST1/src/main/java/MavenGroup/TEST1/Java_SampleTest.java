2105c09e37c106bc5644edf1d923e1d482ac920e
vineel <vnirogi@gmail.com>
2015-10-24 19:00:11 +0530

    Added new Java_SampleTest

---
+++ TEST1/src/main/java/MavenGroup/TEST1/Java_SampleTest.java
@@ -0,0 +1,10 @@
+package MavenGroup.TEST1;
+
+public class Java_SampleTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("New Hello WOrld");
+	}
+
+}
