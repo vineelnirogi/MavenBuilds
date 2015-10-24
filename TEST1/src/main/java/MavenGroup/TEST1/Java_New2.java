ee1e0c39703ea48370e0c839b18acdb8b49047bb
vineel <vnirogi@gmail.com>
2015-10-24 19:02:12 +0530

    Added New JavaClass 2

---
+++ TEST1/src/main/java/MavenGroup/TEST1/Java_New2.java
@@ -0,0 +1,10 @@
+package MavenGroup.TEST1;
+
+public class Java_New2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World 3");
+	}
+
+}
