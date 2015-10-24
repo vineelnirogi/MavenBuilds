4b1736e5981a6503628b2ab85bf71052a757f3e7
vineel <vnirogi@gmail.com>
2015-10-24 19:12:12 +0530

    Hello World 4

--- TEST1/src/main/java/MavenGroup/TEST1/Java_New2.java
+++ TEST1/src/main/java/MavenGroup/TEST1/Java_New2.java
@@ -4,7 +4,7 @@ public class Java_New2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World 3");
+		System.out.println("Hello World 4");
 	}
 
 }
